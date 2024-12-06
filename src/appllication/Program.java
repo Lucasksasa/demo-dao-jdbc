7ac342fb66c28d5fb72b3f5a55da2e5064971093
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2024-12-06 15:40:07 -0300

    SellerDaoJDBC and DaoFactory

--- src/appllication/Program.java
+++ src/appllication/Program.java
@@ -2,6 +2,9 @@ package appllication;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +15,8 @@ public class Program {
 		Department obj= new Department(1, "books");
 		
 		Seller seller= new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
